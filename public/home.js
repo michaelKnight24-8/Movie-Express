25553b60a6f252133df9a6131d9c6f9bd4914740
maknight24 <56240440+maknight24@users.noreply.github.com>
2020-06-19 20:41:28 -0600

    Today's work

--- public/home.js
+++ public/home.js
@@ -1,3 +1,13 @@
+var currentActiveAvatar = "";
+var currentColorTheme = "";
+//holds the container ids
+var containerIds = ["reviews-container", "favorite-movies-container",
+"info-container", "prefs-container"];
+
+//holds slideout ids
+var slideOutIds = ["preferences", "personal-info", "reviews", "your-favorites"];
+
+
 function bodyClick(){
     if($("#sidebar").hasClass('change')){
         toggleSidebar();
@@ -154,20 +164,58 @@ function moveRight(context) {
   document.getElementById(context).scrollLeft -= 50;
 };
 
-function toggleThemes() {
-  document.getElementById("sidebar").classList.toggle("light-theme");
-  document.getElementById("theBody").classList.toggle("light-theme");
+function toggleThemes(color) {
+
+  switch (currentColorTheme) {
+      case "blue":
+        blueTheme();
+        break;
+      case "white":
+        whiteTheme();
+    }
+
+  //the hard part! Find what the current color is, and toggle the color so it
+  //turns off. Then, toggle the correct color on all the different elements,
+  //to match the correct color
+  if  (color.split("-")[0] == "blue") 
+    blueTheme();
+
+  if (color.split("-")[0] == "white") 
+    whiteTheme();
+  if (color.split("-")[0] == "dark")
+    currentColorTheme = "dark";
+  
+}
+
+function whiteTheme() {
+  for (slide of slideOutIds) 
+      document.getElementById(slide).classList.toggle("light-theme");
+  
+
+    document.getElementById("sidebar").classList.toggle("light-theme");
+    document.getElementById("theBody").classList.toggle("light-theme");
+    document.getElementById("top-movies-header").classList.toggle("scrape-theme");
+    document.getElementById("top-shows-header").classList.toggle("scrape-theme");
+
+    document.getElementById("top-shows").classList.toggle("container-theme");
+    document.getElementById("top-movies").classList.toggle("container-theme");
+
+    currentColorTheme = "white";
+}
+
+function blueTheme() {
+  for (slide of slideOutIds) 
+  document.getElementById(slide).classList.toggle("steel-blue-theme");
+
+  document.getElementById('search-header').classList.toggle("blue-headers");
+  document.getElementById("sidebar").classList.toggle("steel-blue-theme");
+  document.getElementById("theBody").classList.toggle("blue-theme");
+  currentColorTheme = "blue"
 }
 
 //function to iterate over all the available containers and slideout menus to open, and shut
 //ones as needed
 function showHidden(context, context2) {
-    //holds the container ids
-    var containerIds = ["reviews-container", "favorite-movies-container",
-                        "info-container", "prefs-container"]
-                        
-    //holds slideout ids
-    var slideOutIds = ["preferences", "personal-info", "reviews", "your-favorites"]
 
     //iterate over conatiners and ids and shut them if needed so there is no overlap
     for (container of containerIds) {
@@ -188,7 +236,7 @@ function showHidden(context, context2) {
     }
 
     document.getElementById(context).classList.toggle("change");
-    document.getElementById(context2).classList.toggle("current");
+    document.getElementById(context2).classList.toggle('current');
 }
 
 function setActive(context) {
@@ -201,4 +249,16 @@ function setActive(context) {
       }
   }
   document.getElementById(context).classList.toggle('active');
+  
+  toggleThemes(context.split("-")[0])
+}
+
+function changeAvatar(avatarId) {
+  if (currentActiveAvatar != "") 
+    document.getElementById(currentActiveAvatar).classList.toggle('current-avatar');
+
+  document.getElementById(avatarId).classList.toggle('current-avatar');
+  currentActiveAvatar = avatarId;
+
+  document.getElementById("img").style["background-image"] = 'url(../images/' + avatarId + '.png)';
 }
